a07a6c9f08e74aae1448d13db8b4167bb95c320c
thiagoperess <thiagoperesschweitzer@gmail.com>
2021-01-23 23:47:32 -0300

    op. invalida jokenpo exe045

--- mundoDois/exercicios/condicoesAninhadas/exe045.py
+++ mundoDois/exercicios/condicoesAninhadas/exe045.py
@@ -10,21 +10,33 @@ print('''Suas opções
 [2] TESOURA''')
 
 import random
+
 humanChoice = int(input('Qual é sua jogada? '))
 pcOptions = [0, 1, 2]
-pedra = pcOptions[0]
-papel = pcOptions[1]
-tesoura = pcOptions[2]
+itens = ('Pedra', 'Papel', 'Tesoura')
 pcChoice = int(random.choice(pcOptions))
 
 if humanChoice == 0 and pcChoice == 2 or \
-    humanChoice == 1 and pcChoice == 0 or \
-    humanChoice == 2 and pcChoice == 1:
-    print('Eu escolhi {} e você {}! \n'
-          'Você ganhou!! PARABÉNS!!'.format(humanChoice, pcChoice))
+        humanChoice == 1 and pcChoice == 0 or \
+        humanChoice == 2 and pcChoice == 1:
+
+    print('Você jogou {}'.format(itens[humanChoice]))
+    print('Computador jogou {}'.format(itens[pcChoice]))
+    print('Você ganhou!! PARABÉNS!!'.format(humanChoice, pcChoice))
+
 elif humanChoice == pcChoice:
-    print('Eu escolhi {} e você {}! \n'
-          'EMPATE! Jogue novamente!'.format(humanChoice, pcChoice))
+
+    print('Você jogou {}'.format(itens[humanChoice]))
+    print('Computador jogou {}'.format(itens[pcChoice]))
+    print('EMPATE! Jogue novamente!'.format(humanChoice, pcChoice))
+
+elif pcChoice == 0 and humanChoice == 2 or \
+        pcChoice == 1 and humanChoice == 0 or \
+        pcChoice == 2 and humanChoice == 1:
+
+    print('Você jogou {}'.format(itens[humanChoice]))
+    print('Computador jogou {}'.format(itens[pcChoice]))
+    print('Você perdeu!! Tente novamente!'.format(humanChoice, pcChoice))
+
 else:
-    print('Eu escolhi {} e você {}! \n'
-          'Você perdeu!! Tente novamente!'.format(humanChoice, pcChoice))
+    print('Não te entendi. Digite uma opção válida!')
